47aca7449e021a84cc9031415d82811df52640d8
vamshidhar-kasulabada <Vamshidhar.Kasulabada@thomsonreuters.com>
2025-08-13 01:10:40 +0530

    nvim: add comments in java lsp setup

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,4 +1,10 @@
-local os = vim.loop.os_uname().sysname -- 'Windows_NT', 'Linux', 'Darwin'
+-- start of lsp setup
+-- [[ prerequisites
+-- 1. Install Java 21 and set the JAVA_21 environment variable to the path
+--    of the Java 21 executable.
+-- 2. Install jdtls using Mason.nvim or manually.
+-- ]]
+local os_name = vim.loop.os_uname().sysname -- 'Windows_NT', 'Linux', 'Darwin'
 local home = os.getenv("HOME")
 local java21 = os.getenv("JAVA_21")
 local nvim_app_data_dir = home .. "/.local/share/nvim"
@@ -6,72 +12,72 @@ local jdtls_workspace_dir = nvim_app_data_dir .. "/jdtls-workspace"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = jdtls_workspace_dir .. "/" .. project_name
 
-
 local status, jdtls = pcall(require, "jdtls")
 if not status then
-	return
+    return
 end
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
 
-if os == "Windows_NT" then
-	os = "win"
-elseif os == "Linux" then
-	os = "linux"
+if os_name == "Windows_NT" then
+    os_name = "win"
+elseif os_name == "Linux" then
+    os_name = "linux"
 else
-	os = "mac"
+    os_name = "mac"
 end
 
 local config = {
-	cmd = {
+    cmd = {
         java21,
-		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-		"-Dosgi.bundles.defaultStartLevel=4",
-		"-Declipse.product=org.eclipse.jdt.ls.core.product",
-		"-Dlog.protocol=true",
-		"-Dlog.level=ALL",
-		"-Xmx1g",
-		"--add-modules=ALL-SYSTEM",
-		"--add-opens",
-		"java.base/java.util=ALL-UNNAMED",
-		"--add-opens",
-		"java.base/java.lang=ALL-UNNAMED",
-		"-javaagent:" .. nvim_app_data_dir .. "/mason/packages/jdtls/lombok.jar",
-		"-jar",
-		vim.fn.glob(nvim_app_data_dir .. "/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-		"-configuration",
-		nvim_app_data_dir .. "/mason/packages/jdtls/config_"..os,
-		"-data",
-		workspace_dir,
-	},
-	root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
-	on_attach = require("vamshidhar.plugins.lsp-config.lsp_config_on_attach").on_attach_java,
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-javaagent:" .. nvim_app_data_dir .. "/mason/packages/jdtls/lombok.jar",
+        "-jar",
+        vim.fn.glob(nvim_app_data_dir .. "/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+        "-configuration",
+        nvim_app_data_dir .. "/mason/packages/jdtls/config_" .. os_name,
+        "-data",
+        workspace_dir,
+    },
+    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+    on_attach = require("vamshidhar.plugins.lsp-config.lsp_config_on_attach").on_attach_java,
 
-	settings = {
-		java = {
-			signatureHelp = { enabled = true },
-			extendedClientCapabilities = extendedClientCapabilities,
-			maven = {
-				downloadSources = true,
-			},
-			referencesCodeLens = {
-				enabled = true,
-			},
-			references = {
-				includeDecompiledSources = true,
-			},
-			inlayHints = {
-				parameterNames = {
-					enabled = "all", -- literals, all, none
-				},
-			},
-			format = {
-				enabled = true,
-			},
-		},
-	},
+    settings = {
+        java = {
+            signatureHelp = { enabled = true },
+            extendedClientCapabilities = extendedClientCapabilities,
+            maven = {
+                downloadSources = true,
+            },
+            referencesCodeLens = {
+                enabled = true,
+            },
+            references = {
+                includeDecompiledSources = true,
+            },
+            inlayHints = {
+                parameterNames = {
+                    enabled = "all", -- literals, all, none
+                },
+            },
+            format = {
+                enabled = true,
+            },
+        },
+    },
 
-	init_options = {
-		bundles = {},
-	},
+    init_options = {
+        bundles = {},
+    },
 }
 require("jdtls").start_or_attach(config)
+-- end of lsp setup
